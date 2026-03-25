f448d00cd3e21f42fc2a86e260a652fd4b22ca71
Aaron Praise <33963243+AARON-PRAISE@users.noreply.github.com>
2026-03-25 19:03:40 +0100

    test

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
-
 const app = express();
+
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/health', (req, res) => {
   res.json({ ok: true });
+});
+
+// TEMP test route
+app.post('/test', (req, res) => {
+  console.log('Body:', req.body);
+  res.json({ received: true });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
